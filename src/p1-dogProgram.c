aed64e57f9366c6956984a07ca5aa082cd5aba1a
Julián Salomón <jesalomont@unal.edu.co>
2017-02-17 22:44:56 -0500

    Write to file and read from file and splitted printof Animals

--- src/p1-dogProgram.c
+++ src/p1-dogProgram.c
@@ -3,100 +3,131 @@
 #include <string.h>
 
 struct Animal {
-  char    nombre[32];
-  char    tipo[32];
-  int     edad;
-  char    raza[16];
-  int     estatura;
-  float   peso;
-  char    sexo;
+  char nombre[32];
+  char tipo[32];
+  int edad;
+  char raza[16];
+  int estatura;
+  float peso;
+  char sexo;
 };
 
-void add_reg( );
+char leerSexo() {
+  char tipo;
+  scanf("%s", &tipo);
+  if (strpbrk(&tipo, "hH")) {
+    return 'H';
+  } else if (strpbrk(&tipo, "mM")) {
+    return 'M';
+  } else {
+    printf("Solo se puede usar H (Hembra)o M (Macho): ");
+    return leerSexo();
+  }
+};
 
-char validar_FM(){//Nos aseguramos que sólo se permitan los dos caracteres H y M.
-    char tipo;
-    scanf( "%c", &tipo);
-    if(tipo=='H' || tipo=='h'){
-        return 'H';
+struct Animal leerAnimal(struct Animal *animal_nuevo){
+  printf("Nombre: ");
+  scanf("%s", &animal_nuevo->nombre);
+  printf("Tipo: ");
+  scanf("%s", &animal_nuevo->tipo);
+  printf("Edad: ");
+  scanf("%d", &animal_nuevo->edad);
+  printf("Raza: ");
+  scanf("%s", &animal_nuevo->raza);
+  printf("Estatura: ");
+  scanf("%d", &animal_nuevo->estatura);
+  printf("Peso: ");
+  scanf("%f", &animal_nuevo->peso);
+  printf("Sexo: ");
+  animal_nuevo->sexo = leerSexo();
+};
+
+void ingresarRegistro() {
+  struct Animal* animal_nuevo;
+  int num = 0;
+
+  animal_nuevo = malloc(sizeof(*animal_nuevo));
+  leerAnimal(animal_nuevo);
+
+  FILE* dataFile = fopen("animales.dat", "r+");
+  fscanf(dataFile, "%d", &num);
+  if(num >= 0){
+    num++;
+  } else {
+    num = 1;
+  }
+  fseek(dataFile, 0, SEEK_SET);
+  fprintf(dataFile, "%d\n", num);
+  fclose(dataFile);
+  dataFile = fopen("animales.dat", "a");
+  fseek(dataFile, 0, SEEK_END);
+  fprintf(dataFile, "%s %s %d %s %d %f %c\n", &animal_nuevo->nombre, &animal_nuevo->tipo, animal_nuevo->edad, &animal_nuevo->raza, animal_nuevo->estatura, animal_nuevo->peso, animal_nuevo->sexo);
+  fclose(dataFile);
+
+  free(animal_nuevo);
+};
+
+void verRegistro(){
+  //struct Animal* animal_existente;
+  int num = 0, cantidad = 0;
+  printf("Indique el número de registro correspondiente al animal que desea ver: ");
+  scanf("%i", &num);
+  char line[150];
+  FILE* dataFile = fopen("animales.dat", "r");  
+  cantidad = atoi(fgets(line, 150, dataFile));
+  if(cantidad >= num){
+    while (num>0){
+      fgets(line, 150, dataFile);
+      num--;
     }
-    else if(tipo=='M' || tipo=='m'){
-        return 'M';
+    char *nombre = strtok(line, " ");
+    printf("Nombre: %s\n", nombre);
+    char *tipo = strtok(NULL, " ");
+    printf("Tipo: \t%s\n", tipo);
+    char *edad = strtok(NULL, " ");
+    printf("Edad: \t%s\n", edad);
+    char *raza = strtok(NULL, " ");
+    printf("Raza: \t%s\n", raza);
+    char *estatura = strtok(NULL, " ");
+    printf("Estatura: %s\n", estatura);
+    char *peso = strtok(NULL, " ");
+    printf("Peso: \t%s\n", peso);
+    char *sexo = strtok(NULL, " ");
+    printf("Sexo: \t%s", sexo);
+  } else {
+    printf("El registro no existe");
+  }  
+  fclose(dataFile);
+};
+
+
+int main() {
+  int opc;
+
+  do {
+    printf("\n==========================");
+    printf("\n|\t.:|Menú|:.\t |");
+    printf("\n==========================");
+    printf("\n| 1. Ingresar registro\t |");
+    printf("\n| 2. Ver registro\t |");
+    printf("\n| 3. Borrar registro\t |");
+    printf("\n| 4. Buscar registro\t |");
+    printf("\n| 5. Salir\t\t |");
+    printf("\n==========================");
+    printf("\nOpcion: ");
+    scanf("%i", &opc);
+    switch (opc) {
+      case 1:
+        ingresarRegistro();
+        break;
+      case 2:
+        verRegistro();
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
     }
-    else{
-        printf( "Solo se puede usar H (Hembra)o M (Macho): " );
-        return validar_FM();
-    }
-}
 
-
-void add_reg(){
-
-	struct Animal *new_animal;
-	int num=0;
-		
-	new_animal = malloc(sizeof(*new_animal));
-
-	printf("Nombre: ");
-	scanf("%s",&new_animal->nombre);
-	printf("Tipo: ");
-	scanf("%s",&new_animal->tipo);
-	printf("Edad: ");
-	scanf("%d",&new_animal->edad);
-	printf("Raza: ");
-	scanf("%s",&new_animal->raza);
-	printf("Estatura: ");
-	scanf("%d",&new_animal->estatura);
-	printf("Peso: ");
-	scanf("%f",&new_animal->peso);	
-	printf("Sexo: ");
-	new_animal->sexo=(char)validar_FM();
-	
-	  //abrimos el archivo
-	FILE * fileInfo = fopen( "bin/animales.dat", "r+");
-	  //leemos el numero de registros
-	fscanf( fileInfo, "%d", &num );
-	  //lo aumentamos en 1
-	num++;
-	 // entra al archivo desde el inicio e imprime el numero en el archivo	
-	fseek( fileInfo, 0, SEEK_SET );
-    	fprintf( fileInfo, "%d\n", num );
-    	fclose( fileInfo );
-
-	 //Abrimos el archivo de datos para agregar el nuevo registro.
- 	fileInfo = fopen( "bin/animales.dat", "a+");
-    	fseek( fileInfo, 0, SEEK_END );
-    	fprintf( fileInfo, "%s %s %d %s %d %f %c\n", &new_animal -> nombre, new_animal -> tipo,
-  	   &new_animal -> edad, &new_animal -> raza, &new_animal -> estatura, &new_animal -> peso, &new_animal -> sexo);
-    	fclose( fileInfo );
-
-  	//Liberamos la memoria reservada para la estructura.
-    	free( new_animal );
-}
-
-
-int main(){
-	
-	
-	int opc;
-	
-	do{
-		printf("\n\t.:MENU:.");
-		printf("\n1. Ingresar registro");
-		printf("\n2. Ver registro");
-		printf("\n3. Borrar registro");
-		printf("\n4. Buscar registro");
-		printf("\n4. Salir");
-		printf("\nOpcion: ");
-		scanf("%i",&opc);
-		
-		switch(opc){
-			case 1: add_reg();break;
-			case 2: break;
-			case 3: break;
-			case 4: break;
-		}
-		
-	}while(opc != 5);
-
-}
+  } while (opc != 5);
+};
